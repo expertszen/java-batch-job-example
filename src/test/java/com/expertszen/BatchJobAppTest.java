cdf2ce5e8519c2b6ea4047851960478c9b1251a3
jenkinsfazil <jenkinsfazil@gmail.com>
2025-10-29 09:57:27 +0530

    Update BatchJobAppTest.java

--- src/test/java/com/expertszen/BatchJobAppTest.java
+++ src/test/java/com/expertszen/BatchJobAppTest.java
@@ -6,6 +6,7 @@ import static org.junit.Assert.assertTrue;
 public class BatchJobAppTest {
     @Test
     public void testApp() {
-        assertTrue(true);;
+        aasssertTrue(true);;
+        test
     }
 }
